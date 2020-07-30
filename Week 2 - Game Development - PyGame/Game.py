a7c94f75034251a987d062ceb0a8a86db42f4129
Aryaman Kakad <askvit2000@gmail.com>
2020-07-30 19:06:16 +0530

    Updating Car Image

--- Week 2 - Game Development - PyGame/Game.py
+++ Week 2 - Game Development - PyGame/Game.py
@@ -8,7 +8,7 @@ display_width = 800
 display_height = 600
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('A bit Racey')
+pygame.display.set_caption('Racing Mania')
 
 black = (0,0,0)
 white = (255,255,255)
@@ -22,16 +22,27 @@ def car(x,y):
 
 x =  (display_width * 0.40)
 y = (display_height * 0.75)
+x_change = 0
 
 while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+    
+    x += x_change
 
     gameDisplay.fill(white)
     car(x,y)
 
-        
     pygame.display.update()
     clock.tick(60)
 
